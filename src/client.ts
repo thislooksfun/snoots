79875ab2633fc9b1c1eea00964a9cf0c50141cff
thislooksfun <thislooksfun@repbot.org>
2021-03-10 22:51:39 -0600

    feat: make the client take in options

--- src/client.ts
+++ src/client.ts
@@ -1,2 +1,28 @@
+import type { Credentials } from "./helper/api/creds";
+
+export interface UsernameAuth {
+  username: string;
+  password: string;
+}
+export interface TokenAuth {
+  refreshToken: string;
+}
+export type Auth = UsernameAuth | TokenAuth;
+
+export interface ClientOptions {
+  auth?: Auth;
+  creds?: Credentials;
+  userAgent: string;
+}
+
 export default class Client {
+  protected auth?: Auth;
+  protected creds?: Credentials;
+  protected userAgent: string;
+
+  constructor(opts: ClientOptions) {
+    this.auth = opts.auth;
+    this.creds = opts.creds;
+    this.userAgent = opts.userAgent;
+  }
 }
