3964f7a6c1419e2d49a1fe878e2b4eedec096d82
thislooksfun <thislooksfun@repbot.org>
2021-03-11 00:13:41 -0600

    feat: add a function to update the client's token

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,6 @@
 import type { Credentials } from "./helper/api/creds";
+import type { Token } from "./helper/accessToken";
+import refreshToken from "./helper/accessToken";
 
 export interface UsernameAuth {
   username: string;
@@ -18,11 +20,25 @@ export interface ClientOptions {
 export default class Client {
   protected auth?: Auth;
   protected creds?: Credentials;
+  protected token: Token | null;
   protected userAgent: string;
 
   constructor(opts: ClientOptions) {
     this.auth = opts.auth;
     this.creds = opts.creds;
+    this.token = null;
     this.userAgent = opts.userAgent;
+  }
+
+  protected async updateAccessToken(): Promise<void> {
+    if (!this.auth) throw "No auth";
+    if (!this.creds) throw "No creds";
+
+    this.token = await refreshToken(
+      this.token,
+      this.auth,
+      this.creds,
+      this.userAgent
+    );
   }
 }
