ab68e710c473fcc87ece4f3ab84d3bae913ab1dd
thislooksfun <thislooksfun@repbot.org>
2021-03-11 23:15:08 -0600

    docs: comment Content

--- src/objects/content.ts
+++ src/objects/content.ts
@@ -1,12 +1,17 @@
+/** The base for all the reddit object data types. */
 export interface ContentData {
+  /** The unix timestamp when this object was created.  */
   createdUtc: number;
+  /** The ID of this object. */
   id: string;
 }
 
+/** The base class that all other objects inherit from. */
 export default class Content implements ContentData {
   createdUtc: number;
   id: string;
 
+  /** @internal */
   constructor(data: ContentData) {
     this.createdUtc = data.createdUtc;
     this.id = data.id;
