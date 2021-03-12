671bde925b1e93e7b0ffec53e7e51c1a5250a175
thislooksfun <tlf@thislooks.fun>
2021-03-12 13:52:25 -0600

    docs: mark RedditObject and _Listing as internal

--- src/helper/types.ts
+++ src/helper/types.ts
@@ -1,7 +1,10 @@
 /** A string-keyed object that can hold anything. */
 export type Data = Record<string, any>;
+
+/** @internal */
 export type RedditObject = { kind: string; data: Data };
 
+/** @internal */
 export interface _Listing {
   after: string | null;
   before: string | null;
