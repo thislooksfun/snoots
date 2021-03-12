61b25a2ceb92ea44bcc7037a87ebe4184322d491
thislooksfun <tlf@thislooks.fun>
2021-03-12 12:05:04 -0600

    docs: document Client

--- src/client.ts
+++ src/client.ts
@@ -8,21 +8,92 @@ import * as oauth from "./helper/api/oauth";
 import CommentControls from "./controls/comment";
 import refreshToken from "./helper/accessToken";
 
+/** Username and password based authentication */
 export interface UsernameAuth {
   username: string;
   password: string;
 }
+
+/** OAuth token based authentication */
 export interface TokenAuth {
   refreshToken: string;
 }
+
+/** Some kind of authorization */
 export type Auth = UsernameAuth | TokenAuth;
 
+/**
+ * Options for instantiating a Client
+ */
 export interface ClientOptions {
+  /**
+   * The authorization information for this client to log in with.
+   *
+   * If this is not specified the client will fall back on
+   * [application-only-auth][aoa].
+   *
+   * [aoa]: https://github.com/reddit-archive/reddit/wiki/OAuth2#application-only-oauth
+   */
   auth?: Auth;
+
+  /**
+   * The authorization information for this client to log in with.
+   *
+   * If this is not specified the client will fall back on unauthenticated
+   * requests. Leaving this out is almost never what you want to do.
+   */
   creds?: Credentials;
+
+  /**
+   * The user agent for this client.
+   *
+   * Every reddit application is required to have a unique user agent that
+   * preferably includes the version number.
+   */
   userAgent: string;
 }
 
+/**
+ * The main Client class. This is the primary way you will interact with snoots.
+ *
+ * Every Client instance is fully independent, so you are free to create as many
+ * as you require.
+ *
+ * @example The most common way to use snoots is to make a client
+ * ```ts
+ * const client = new Client({
+ *   userAgent: '<your user agent>',
+ *   auth: {
+ *     username: '<your username>',
+ *     password: '<your password>',
+ *   },
+ *   creds: {
+ *     clientId: '<your client id>',
+ *     clientSecret: '<your client secret>',
+ *   },
+ * })
+ * ```
+ *
+ *
+ * @example If you want to make requests not on behalf of a user, you can leave
+ * out the `auth` key. See {@link ClientOptions.auth} for more details.
+ * ```ts
+ * const client = new Client({
+ *   userAgent: '<your user agent>',
+ *   creds: {
+ *     clientId: '<your client id>',
+ *     clientSecret: '<your client secret>',
+ *   },
+ * });
+ * ```
+ *
+ * @example If you want to make completely unauthenticated requests, you can
+ * leave out both the `auth` and `creds` keys. See {@link ClientOptions.creds}
+ * for more details and restrictions.
+ * ```ts
+ * const client = new Client({ userAgent: '<your user agent>' })
+ * ```
+ */
 export default class Client {
   /** Controls for interacting with comments. */
   public comments: CommentControls;
@@ -31,6 +102,11 @@ export default class Client {
   protected token: Token | null;
   protected userAgent: string;
 
+  /**
+   * Make a new snoots Client.
+   *
+   * @param opts The options to configure this client with.
+   */
   constructor(opts: ClientOptions) {
     this.auth = opts.auth;
     this.creds = opts.creds;
@@ -41,6 +117,20 @@ export default class Client {
     this.comments = new CommentControls(this);
   }
 
+  /**
+   * Perform a GET request to the reddit api.
+   *
+   * You shouldn't ever have to use this directly.
+   *
+   * @template T The type to cast the response to.
+   *
+   * @param path The path of the endpoint.
+   * @param query Any query parameters to pass to the endpoint.
+   *
+   * @returns The JSON response from the endpoint.
+   *
+   * @throws If the endpoint returns an error.
+   */
   async get<T>(path: string, query: Query = {}): Promise<T> {
     if (this.creds) {
       return oauth.get(await this.oAuth(), path, query);
@@ -48,6 +138,22 @@ export default class Client {
       return anon.get(this.userAgent, path, query);
     }
   }
+
+  /**
+   * Perform a POST request to the reddit api.
+   *
+   * You shouldn't ever have to use this directly.
+   *
+   * @template T The type to cast the response to.
+   *
+   * @param path The path of the endpoint.
+   * @param data The data to POST.
+   * @param query Any query parameters to pass to the endpoint.
+   *
+   * @returns The JSON response from the endpoint.
+   *
+   * @throws If the endpoint returns an error.
+   */
   async post<T>(path: string, data: Data, query: Query = {}): Promise<T> {
     if (this.creds) {
       return oauth.post(await this.oAuth(), path, data, query);
