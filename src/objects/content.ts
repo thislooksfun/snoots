7fd3b7cd1649388ea29d0891b6cd8f51575720ce
thislooksfun <thislooksfun@repbot.org>
2021-03-11 23:12:37 -0600

    refactor: remove unused values
    This removes two useless parameters from all reddit content objects:
    
    1. `created` is a strictly inferior version of `createdUtc`
    2. `name` can be recreated with the `id` and type, and doesn't need
       to be exposed to the user.

--- src/objects/content.ts
+++ src/objects/content.ts
@@ -1,20 +1,14 @@
 export interface ContentData {
-  created: number;
   createdUtc: number;
   id: string;
-  name: string;
 }
 
 export default class Content implements ContentData {
-  created: number;
   createdUtc: number;
   id: string;
-  name: string;
 
   constructor(data: ContentData) {
-    this.created = data.created;
     this.createdUtc = data.createdUtc;
     this.id = data.id;
-    this.name = data.name;
   }
 }
