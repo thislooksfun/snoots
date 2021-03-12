0f4893dea9f3239f0d8c5b226e576ccd8b79a39d
thislooksfun <tlf@thislooks.fun>
2021-03-12 14:31:38 -0600

    docs: export Listing and EachFn

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { default as default } from "./client";
 export * from "./client";
 
+export { default as Listing, EachFn } from "./listings/listing";
+
 export { Data } from "./helper/types";
 export { Query } from "./helper/api/core";
 export { Credentials } from "./helper/api/creds";
