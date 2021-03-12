55717b7029dd9a360aeb2ce1632cd7fd404c7d44
thislooksfun <tlf@thislooks.fun>
2021-03-12 15:18:38 -0600

    ci: work towards automated alpha releases

--- release.config.js
+++ release.config.js
@@ -1,13 +1,15 @@
 // semantic-release config
-const commitMsg = "chore(release): ${nextRelease.version} [skip ci]";
+// const commitMsg = "chore(release): ${nextRelease.version} [skip ci]";
 module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    ["@semantic-release/npm", { tarballDir: "release" }],
-    ["@semantic-release/github", { assets: "release/*.tgz" }],
-    ["@semantic-release/git", { message: commitMsg }],
+    // ["@semantic-release/npm", { tarballDir: "release" }],
+    // ["@semantic-release/github", { assets: "release/*.tgz" }],
+    // ["@semantic-release/git", { message: commitMsg }],
   ],
   preset: "conventionalcommits",
+  // TODO: This should be changed as we move to beta.
+  branches: [{ name: "master", channel: "alpha", prerelease: "alpha" }],
 };
