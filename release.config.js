d3fe5f709eb688a3c0f8f0de659760a2743d0908
thislooksfun <tlf@thislooks.fun>
2021-03-12 16:54:19 -0600

    ci: re-disable semantic-release
    The repo is now set up to automatically publish v1.0.0-alpha.1,
    but it is not yet time to do so, so I'm turning it off for now.

--- release.config.js
+++ release.config.js
@@ -1,13 +1,13 @@
 // semantic-release config
-// const commitMsg = "chore(release): ${nextRelease.version} [skip ci]";
+const commitMsg = "chore(release): ${nextRelease.version} [skip ci]";
 module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    // ["@semantic-release/npm", { tarballDir: "release" }],
-    // ["@semantic-release/github", { assets: "release/*.tgz" }],
-    // ["@semantic-release/git", { message: commitMsg }],
+    ["@semantic-release/npm", { tarballDir: "release" }],
+    ["@semantic-release/github", { assets: "release/*.tgz" }],
+    ["@semantic-release/git", { message: commitMsg }],
   ],
   preset: "conventionalcommits",
   // TODO: This should be changed as we move to beta.
