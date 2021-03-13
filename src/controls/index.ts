96c4a16725126854b9fd3243d7068d4e808b419b
thislooksfun <tlf@thislooks.fun>
2021-03-12 22:37:47 -0600

    docs: export CommentControls

---
+++ src/controls/index.ts
@@ -0,0 +1,1 @@
+export { default as CommentControls } from "./comment";
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { default as default } from "./client";
 export * from "./client";
 
 export { default as Listing, EachFn } from "./listings/listing";
+export * from "./controls";
 
 export { Data } from "./helper/types";
 export { Query } from "./helper/api/core";
