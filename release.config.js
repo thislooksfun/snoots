84c5184ad0a2120a1d0b063dba23c60f8100ccbb
thislooksfun <tlf@thislooks.fun>
2021-03-13 20:56:12 -0600

    ci: configure to release v1.0.0-dev.1

--- release.config.js
+++ release.config.js
@@ -4,10 +4,10 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    // "@semantic-release/changelog",
-    // ["@semantic-release/npm", { tarballDir: "release" }],
-    // ["@semantic-release/github", { assets: "release/*.tgz" }],
-    // ["@semantic-release/git", { message: commitMsg }],
+    "@semantic-release/changelog",
+    ["@semantic-release/npm", { tarballDir: "release" }],
+    ["@semantic-release/github", { assets: "release/*.tgz" }],
+    ["@semantic-release/git", { message: commitMsg }],
   ],
   preset: "conventionalcommits",
   // TODO: This should be changed as we move to beta.
