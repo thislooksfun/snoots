686933ba9e2d3c760be27de9ad608bd69968b8dd
thislooksfun <tlf@thislooks.fun>
2022-02-03 00:41:43 -0600

    docs: improve documentation

--- src/helper/types.ts
+++ src/helper/types.ts
@@ -2,10 +2,21 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Data = Record<string, any>;
 
-/** Either a type or a promise that resolves to that type. */
+/**
+ * Either a type or a promise that resolves to that type.
+ *
+ * @template T The resolved value type.
+ */
 export type Awaitable<T> = T | Promise<T>;
 
-/** A function that either returns a type or a promise of that type. */
+/**
+ * A function that either returns a type or a promise of that type.
+ *
+ * @template T The input type.
+ * @template R The return type.
+ * @param t Some input value.
+ * @returns An Awaitable value.
+ */
 export type AwaitableFunction<T, R> = (t: T) => Awaitable<R>;
 
 /** A size, in the form [width, height]. */
@@ -40,5 +51,9 @@ export type UserItemsSort = "controversial" | "hot" | "new" | "rising" | "top";
 /** @internal */
 export type RedditObject<T = Data> = { kind: string; data: T };
 
-/** A value that may or may not be `undefined`. */
+/**
+ * A value that may or may not be `undefined`.
+ *
+ * @template T The type to be made optional.
+ */
 export type Maybe<T> = T | undefined;
