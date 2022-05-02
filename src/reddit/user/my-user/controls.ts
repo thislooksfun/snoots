48f41c02eb007aeb816d104bb41e182a3a9027f9
thislooksfun <tlf@thislooks.fun>
2022-05-02 01:07:31 -0500

    feat: add MyUserControls::getContributorSubreddits()

--- src/reddit/user/my-user/controls.ts
+++ src/reddit/user/my-user/controls.ts
@@ -1,4 +1,6 @@
 import type { Data } from "../../../helper/types";
+import type { Listing } from "../../listing/listing";
+import type { Subreddit } from "../../subreddit/object";
 import type { RedditObject } from "../../types";
 import type { MyUser } from "./object";
 
@@ -20,5 +22,22 @@ export class MyUserControls extends BaseUserControls {
     // /me doesn't return a wrapped object, so we have to make it ourselves.
     const raw: RedditObject = { kind: "t2", data: userData };
     return this.client.users.fromRaw(raw) as MyUser;
+  }
+
+  /** @internal */
+  protected getMySubreddits(where: string): Listing<Subreddit> {
+    return this.client.subreddits["getSubreddits"](`mine/${where}`);
+  }
+
+  /**
+   * Get the subreddits the authenticated account is a contributor in.
+   *
+   * @note Due to the way Reddit implements Listings, this will only contain the
+   * first 1000 subreddits.
+   *
+   * @returns A Listing of Subreddits.
+   */
+  getContributorSubreddits(): Listing<Subreddit> {
+    return this.getMySubreddits("contributor");
   }
 }
